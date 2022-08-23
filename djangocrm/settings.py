703e5a079802b6605137a0f814e0f57d7dbd9c42
etchichuryg <ginettetchichury@gmail.com>
2022-08-23 23:05:41 +0200

    Filter Search

--- djangocrm/settings.py
+++ djangocrm/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts',
     'static',
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
