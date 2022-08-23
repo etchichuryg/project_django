f97de9e0ed61f46d7ad4f0741d4c673476fc9018
etchichuryg <ginettetchichury@gmail.com>
2022-08-23 22:35:45 +0200

    Inline Formsets

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
+
 
 
 def home(request):
@@ -32,16 +34,20 @@ def customer(request, pk_test):
     context = {'customer': customer, 'orders': orders, 'order_count':order_count}
     return render(request, 'accounts/customer.html', context)
 
-def createOrder(request):
-    form = OrderForm()
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=5)
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+    #form = OrderForm(initial={'customer':customer})
     if request.method == 'POST':
         #print('Printing POST:', request.POST)
-        form = OrderForm (request.POST)
-        if form.is_valid():
-            form.save()
+        #form = OrderForm (request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
 
-    context = {'form': form }
+    context = {'formset': formset }
     return render(request, 'accounts/order_form.html', context)
 
 def updateOrder(request, pk): 
@@ -63,6 +69,6 @@ def deleteOrder(request, pk):
     if request.method == "POST":
         order.delete()
         return redirect('/')
-        
+
     context ={'item': order}
     return render (request, 'accounts/delete.html', context)
